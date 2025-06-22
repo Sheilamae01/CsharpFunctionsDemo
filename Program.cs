250caad99278b80c8f3992534aeb762177bc175c
orapa <orapa@SHEILA>
2025-06-22 23:04:08 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace CSharpFunctionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Function Demo ===");
+            GreetUser("Sheila");
+            int result = AddNumbers(10, 20);
+            Console.WriteLine($"The result of addition is: {result}");
+        }
+
+        static void GreetUser(string name)
+        {
+            Console.WriteLine($"Hello, {name}! Welcome to C# functions.");
+        }
+
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
